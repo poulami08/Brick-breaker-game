edb090fd5a520ff97d374dcfca2d8d48a33b2a54
Poulami Mandal <poulami.kv@gmail.com>
2022-03-07 22:21:54 +0530

    4th commit

--- gameplay.java
+++ gameplay.java
@@ -4,12 +4,13 @@ import javax.swing.Timer;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.awt.Graphics;
-import java.awt.Color;
+//import java.awt.Graphics;
+//import java.awt.Color;
+import java.awt.*;
 public class gameplay extends JPanel implements KeyListener , ActionListener{
     private boolean play = false;
     private int score = 0;
-    private int totalbricks = 21;
+    private int totalBricks = 21;
     private Timer timer;
     private int delay = 8;
     private int playerX = 310;
@@ -18,7 +19,10 @@ public class gameplay extends JPanel implements KeyListener , ActionListener{
     private int ballXdir = -1;
     private int ballYdir = -2;
 
+    private mapgenerator map;
+
     public gameplay() {
+        map = new mapgenerator(3, 7);
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
@@ -31,6 +35,9 @@ public class gameplay extends JPanel implements KeyListener , ActionListener{
         //bckground
         g.setColor(Color.black);
 		g.fillRect(1, 1, 692, 592);
+
+        // drawing map
+		map.draw((Graphics2D) g);
 
         // borders
 		g.setColor(Color.yellow);
@@ -57,6 +64,55 @@ public class gameplay extends JPanel implements KeyListener , ActionListener{
     public void actionPerformed(ActionEvent e) {
         timer.start();
         if(play){
+            if(new Rectangle(ballposX, ballposY, 20, 20).intersects(new Rectangle(playerX, 550, 100, 8)))
+			{
+				ballYdir = -ballYdir;
+				//ballXdir = -2;
+			}
+			//  else if(new Rectangle(ballposX, ballposY, 20, 20).intersects(new Rectangle(playerX + 70, 550, 30, 8)))
+			//  {
+			//  	ballYdir = -ballYdir;
+			//  	ballXdir = ballXdir + 1;
+		    //  }
+			//  else if(new Rectangle(ballposX, ballposY, 20, 20).intersects(new Rectangle(playerX + 30, 550, 40, 8)))
+			//  {
+			//  	ballYdir = -ballYdir;
+			//  }
+
+            A:for(int i = 0; i<map.map.length; i++)
+			{
+				for(int j =0; j<map.map[0].length; j++)
+				{	
+                    if(map.map[i][j] > 0)
+					{
+                        int brickX = j * map.brickWidth + 80;
+						int brickY = i * map.brickHeight + 50;
+						int brickWidth = map.brickWidth;
+						int brickHeight = map.brickHeight;
+                        Rectangle rect = new Rectangle(brickX, brickY, brickWidth, brickHeight);					
+						Rectangle ballRect = new Rectangle(ballposX, ballposY, 20, 20);
+						Rectangle brickRect = rect;
+                        if(ballRect.intersects(brickRect))
+						{					
+							map.setBrickValue(0, i, j);
+							totalBricks--;
+                            score+=5;
+                            // when ball hit right or left of brick
+							if(ballposX + 19 <= brickRect.x || ballposX + 1 >= brickRect.x + brickRect.width)	
+							{
+								ballXdir = -ballXdir;
+							}
+							// when ball hits top or bottom of brick
+							else
+							{
+								ballYdir = -ballYdir;				
+							}
+                            break A;
+							
+						}
+                    }
+                }
+            }
             ballposX += ballXdir;
 			ballposY += ballYdir;
 			
